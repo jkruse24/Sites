ab581a8cffb185c03cb334291bfd211bb373eafb
Jake E. Kruse <jakekruse2@gmail.com>
2016-05-20 18:06:40 -0400

    Added sample Blog project with Article model, User Model, Comment Model, in dev Session Model.

---
+++ blog/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+	if @user.save
+      flash[:success] = "Welcome to the Blog Site!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+    private #Practice an extra layer of indentation on private methods (distinction)
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+      end
+end
